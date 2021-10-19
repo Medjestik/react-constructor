6cd89a5633ae7251397baba5d91c126e7c652beb
Kostyulin Ivan <ivan-kostyulin@yandex.ru>
2021-10-19 11:43:50 +0300

    fix: develop subtitle

--- src/components/Development/Dpp/Approval/Approval.js
+++ src/components/Development/Dpp/Approval/Approval.js
@@ -61,7 +61,6 @@ function Approval({ dppDescription, loggedIn }) {
   return (
     <div className="approval">
       <h1 className="main__title">Утверждение программы</h1>
-      <p className="main__subtitle">Этап находится в разработке.</p> 
 
       {
         isLoadingData
