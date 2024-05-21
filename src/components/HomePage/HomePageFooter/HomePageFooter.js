1161abe9f82f0e54feef0937d665d575f74699c0
Medjestik <ivan-kostyulin@yandex.ru>
2024-05-21 12:13:34 +0300

    refactor: footer year & contact

--- src/components/HomePage/HomePageFooter/HomePageFooter.js
+++ src/components/HomePage/HomePageFooter/HomePageFooter.js
@@ -25,13 +25,13 @@ function HomePageFooter() {
             <li className="footer__column">
               <h4 className="footer__column-title">Контакты</h4>
               <ul className="footer__column-links">
+                <li className="footer__column-item">Почта для получения доступа:</li>
                 <li className="footer__column-item">ief07@bk.ru</li>
-                <li className="footer__column-item">+7 (499) 653-55-16</li>
               </ul>
             </li>
           </ul>
         </div>
-        <span className="footer__copy">&copy;2021 Все права защищены, ИЭФ&nbsp;РУТ&nbsp;(МИИТ)</span>
+        <span className="footer__copy">&copy;{new Date().getFullYear()} Все права защищены, ИЭФ&nbsp;РУТ&nbsp;(МИИТ)</span>
       </div>
     </footer>
   );
