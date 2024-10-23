6f76d4b79fda85227d50d5798043eb3a323dc747
Medjestik <ivan-kostyulin@yandex.ru>
2024-10-23 13:42:02 +0300

    fix: video links

--- src/components/HomePage/HomePageHeader/HomePageHeader.js
+++ src/components/HomePage/HomePageHeader/HomePageHeader.js
@@ -10,11 +10,11 @@ function HomePageHeader({ onOpenLoginPopup }) {
       <div className="container">
         <nav className="header-homepage__nav">
           <a className="header-homepage__logo-link" href="https://www.miit.ru/" target="_blank" rel="noreferrer"><img className="header-homepage__logo" src={logo} alt="logo"></img></a>
-          <ul className="header-homepage__links">
-            <li className="header-homepage__link"><Link to="homepage-capabilities" smooth={true} offset={0} duration= {500}>О конструкторе</Link></li>
-            <li className="header-homepage__link"><Link to="homepage-methodology" smooth={true} offset={0} duration= {1000}>Методология</Link></li>
-            <li className="header-homepage__link"><Link to="footer" smooth={true} offset={0} duration= {1500}>Контакты</Link></li>
-          </ul>
+          <div className="header-homepage__links">
+            <Link className="header-homepage__link" to="homepage-capabilities" smooth={true} offset={0} duration= {500}>О конструкторе</Link>
+            <Link className="header-homepage__link" to="homepage-methodology" smooth={true} offset={0} duration= {1000}>Методология</Link>
+            <Link  className="header-homepage__link" to="footer" smooth={true} offset={0} duration= {1500}>Контакты</Link>
+          </div>
           <button className="header-homepage__button" onClick={onOpenLoginPopup}>Вход в конструктор</button>
         </nav>
       </div>
